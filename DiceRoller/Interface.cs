6e0b4870694066a30bcdd2b51fd87e260f6e18e3
Erol <e.ermis@yahoo.com>
2024-12-29 21:53:28 +0000

    Add project files.

---
+++ DiceRoller/Interface.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    
+    internal class Interface
+    {
+        private RandomNumbers roller;
+        public Interface() {
+            roller = new RandomNumbers();
+        }
+
+        //TODO: Handle Multiple Die Rolls.
+        public void rollDie(int numberOfDice, int numberOfFaces)
+        {
+            Console.WriteLine("Rolling die {0}d{1}: {3}",numberOfDice, numberOfFaces,roller.generateRandomNumber())       
+        }
+    }
+}
---
+++ DiceRoller/RandomNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    internal class RandomNumbers
+    {
+        private Random rnd;
+        public RandomNumbers()
+        {
+            rnd = new Random();
+        }
+
+        public int generateRandomNumber(int upperbound)
+        {
+            return rnd.Next(0, upperbound);
+        }
+
+    }
+
+    
+    }
